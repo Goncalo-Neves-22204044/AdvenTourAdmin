933a3159d025c425d403a93003edfff84f1968eb
Gonçalo Neves <guga.santos.neves@gmail.com>
2025-05-24 00:23:26 +0100

    Add LoginPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
 import './index.css';
 import CreateAttractionForm from './components/CreateAttractionForm.jsx';
+import LoginForm from './components/LoginForm.jsx'; // create this
+import PrivateRoute from './services/PrivateRoute.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<App />} /> */}
-        {/* <Route path="/create" element={<CreateAttractionForm />} /> */}
-        <Route path="/" element={<CreateAttractionForm />} />
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/create" element={
+            <PrivateRoute>
+              <CreateAttractionForm />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-
