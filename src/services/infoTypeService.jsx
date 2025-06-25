5797c042c396766f5d509a3a99b927a52c69db80
Gonçalo Neves <guga.santos.neves@gmail.com>
2025-06-25 18:58:56 +0100

    Fix method name

--- src/services/infoTypeService.jsx
+++ src/services/infoTypeService.jsx
@@ -1,6 +1,6 @@
 import api from './api';
 
-export async function getAllCountries() {
+export async function getAllInfoTypes() {
   const response = await api.get('/Attraction/infoTypes');
   return response.data.data;
 }
